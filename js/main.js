796f747c8ebb0764eb9db91a9d357baf86283072
Farid Rn <faridv@gmail.com>
2013-04-28 23:32:20 +0430

    mobile view

--- js/main.js
+++ js/main.js
@@ -1,16 +1,56 @@
 $(function() {
+	
+	var $window = $(window);
+	
 	$("#searchcategories").select2({
 		dropdownCssClass: 'bigdrop',
 		containerCssClass: 'search-selectbox'
 	});
 
-	// alert($('body').width());
-	
-	$(".nav-list").delegate('li', 'mouseover', function(e) {
-		$(this).find("a").addClass('active');
-		$(this).find("div.sub-items").css({'right': $(this).width() - 30 }).toggle();
-	}).delegate('li', 'mouseout', function(e) {
-		$(this).find('a').removeClass('active');
-		$(this).find("div.sub-items").toggle();
-	});
+	function handleSidenav() {
+		$(".nav-list").delegate('li', 'mouseover', function(e) {
+			$(this).find("a").addClass('active');
+			if ($("html").hasClass('lt-ie8')) $(this).find("div.sub-items").toggle();
+		}).delegate('li', 'mouseout', function(e) {
+			$(this).find('a').removeClass('active');
+			if ($("html").hasClass('lt-ie8')) $(this).find("div.sub-items").toggle();
+		});
+	}
+
+    function checkWidth() {
+        var windowsize = $window.width();
+        if (windowsize < 767) {
+			smallScreenDelegation();
+        } else {
+        	SmallScreenUndelegation();
+        }
+    }
+    checkWidth();
+    handleSidenav();
+    $window.resize(checkWidth);
+    
+    function smallScreenDelegation() {
+    	$(".nav-list").undelegate('li');
+    	$(".nav-list").undelegate('li');
+    	$(".nav-list").delegate('li a:first', 'click', function(event) {
+    		if ($(this).next().is(':hidden')) {
+				$(this).addClass('active');
+				// $(this).next().slideDown('slow');
+			} else {
+				$(this).removeClass('active').next().slideUp('slow');
+			}		    	
+    		event.preventDefault();	
+    	});
+    	$(".nav-list").find(".arrow.icon-left-open").each(function() {
+    		$(this).removeClass('icon-left-open').addClass('icon-down-open');
+    	});
+    }
+    
+    function SmallScreenUndelegation() {
+    	handleSidenav();
+    	$(".nav-list").find(".arrow.icon-down-open").each(function() {
+    		$(this).removeClass('icon-down-open').addClass('icon-left-open');
+    	});
+    }
+    
 });
