bbab7c554954e823d0674d43863f353ab81dec3f
Farid Rn <faridv@gmail.com>
2013-05-01 23:21:20 +0430

    working on responsive sidebar

--- js/main.js
+++ js/main.js
@@ -1,56 +1,67 @@
 $(function() {
 	
 	var $window = $(window);
+	var device;
 	
 	$("#searchcategories").select2({
 		dropdownCssClass: 'bigdrop',
 		containerCssClass: 'search-selectbox'
 	});
 
-	function handleSidenav() {
-		$(".nav-list").delegate('li', 'mouseover', function(e) {
+	function desktopSidenav() {
+		$(".nav-list > li").off('click');
+		if ($(".nav-list").find(".arrow:first").hasClass('icon-down-open')) {
+			$(".nav-list").find(".arrow.icon-down-open").each(function() {
+	    		$(this).removeClass('icon-down-open').addClass('icon-left-open');
+	    	});
+    	}
+		$(".nav-list > li").on('mouseover', function(e) {
 			$(this).find("a").addClass('active');
 			if ($("html").hasClass('lt-ie8')) $(this).find("div.sub-items").toggle();
-		}).delegate('li', 'mouseout', function(e) {
+		}).on('mouseout', function(e) {
 			$(this).find('a').removeClass('active');
 			if ($("html").hasClass('lt-ie8')) $(this).find("div.sub-items").toggle();
 		});
 	}
-
-    function checkWidth() {
-        var windowsize = $window.width();
-        if (windowsize < 767) {
-			smallScreenDelegation();
-        } else {
-        	SmallScreenUndelegation();
-        }
-    }
-    checkWidth();
-    handleSidenav();
-    $window.resize(checkWidth);
-    
-    function smallScreenDelegation() {
-    	$(".nav-list").undelegate('li');
-    	$(".nav-list").undelegate('li');
-    	$(".nav-list").delegate('li a:first', 'click', function(event) {
-    		if ($(this).next().is(':hidden')) {
-				$(this).addClass('active');
-				// $(this).next().slideDown('slow');
+	
+	function handheldSidenav() {
+		$(".nav-list > li").off('mouseover').off('mouseout');
+		$(".nav-list").find(".arrow.icon-left-open").each(function() {
+    		$(this).removeClass('icon-left-open').addClass('icon-down-open');
+    	});
+		$(".nav-list > li").on('click', function(event) {
+				// $(this).find('a:first').addClass('active');
+    		if ($(this).find('div.sub-items').is(':hidden')) {
+				$(this).find('a:first').addClass('active').next().slideDown('slow');
 			} else {
-				$(this).removeClass('active').next().slideUp('slow');
+				$(this).find('a:first').removeClass('active').next().slideUp('slow');
 			}		    	
     		event.preventDefault();	
     	});
-    	$(".nav-list").find(".arrow.icon-left-open").each(function() {
-    		$(this).removeClass('icon-left-open').addClass('icon-down-open');
-    	});
-    }
-    
-    function SmallScreenUndelegation() {
-    	handleSidenav();
-    	$(".nav-list").find(".arrow.icon-down-open").each(function() {
-    		$(this).removeClass('icon-down-open').addClass('icon-left-open');
-    	});
-    }
+	}
+
+	if ($window.width() > 767) {
+		device = 'desktop';
+		desktopSidenav();
+	} else {
+		device = 'handheld';
+		handheldSidenav();
+	}
+	$(".slideshow").text($window.width());
+	
+    $window.resize(function() {
+    	if ($window.width() > 767) {
+			if (device = 'handheld') {
+				device = 'desktop';
+				desktopSidenav();
+			}
+    	} else {
+			if (device = 'desktop') {
+				device = 'handheld';
+				handheldSidenav();
+			}
+    	}
+		$(".slideshow").text($window.width() + ' ' + device);
+    });
     
 });
