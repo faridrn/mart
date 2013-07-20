d62d671193d1b6c5a91e0c7e1a62fdf49c7b16d7
Farid Rn <faridv@gmail.com>
2013-07-20 22:30:25 +0430

    Rewriting javascript code and use object oriented

--- js/main.js
+++ js/main.js
@@ -8,17 +8,20 @@ $(function() {
 	var shipping = ['با هزینه مشتری', 'با هزینه فروشنده'];
 	var dTemp = $("#details-template").html();
 	$(".zoom").zoom({url: $(".zoom").find('img:first').attr('data-zoom-image')});
-	
+	$("body").on('hidden', '.modal', function () { $(this).removeData('modal'); });
 	$("#cart-link").click(function(e) {
-		$("#cart-modal").modal('toggle');
-		e.preventDefault();
+		cart.toggleModal(e);
 	});
 	
-	$checkboxes = $("input[name=check]");
-	$("#cart-modal").find("input.checkall").click(function() {
-		$checkboxes.prop("checked", !$checkboxes.prop("checked"));
-	});
 	
+	
+	
+	/*function initAddCart() {
+		$("#add-cart, #add-cart a").click(function(e) {
+			
+			e.preventDefatult();
+		});
+	}*/
 	
 	$("#login-link").popover({
 		html: true
@@ -37,136 +40,129 @@ $(function() {
 		}
 	});
 	
-	function loadDetails(id) {
-		var d = false;
-		if (typeof(id) != "undefined" && id) {
-			$.ajax({
-				url: 'data/details.html',
-				success: function(data) {
-					d = data;
-					alert('a');
-				}
+	var cart = {
+		cartModal: $("#cart-modal"),
+		$checkboxes: this.cartModal.find("input[name=check]"),
+		toggleModal: function(e) {
+			this.cartModal.modal('toggle');
+			this.handleCheckboxes();
+			e.preventDefault();
+		},
+		handleCheckboxes: function() {
+			this.cartModal.find("input.checkall").click(function() {
+				$checkboxes.prop("checked", !$checkboxes.prop("checked"));
 			});
 		}
-		return d;
-	}
+		
+	};
 	
-	function constructItems() {
-		$("#items").masonry({
-			itemSelector: '.item',
-			isRTL: true,
-			isAnimated: !Modernizr.csstransitions
-		});
-	}
-	
-	function loadItems() {
-		output = '';
-		$.ajax({
-			url: 'data/items.php',
-			dataType: 'json',
-			cache: false,
-			success: function(data) {
-				// console.log(data);
-				for (var i = 0; i < data.length; i++) {
-					// console.log(data[i]);
-					output += '<div class="item" data-id="' + data[i].id + '">';
-						output += '<div class="inner">';
-							output += '<div class="image">';
-								output += '<img src="' + data[i].image + '" alt="' + data[i].imagealt + '" />';
-							output += '</div>';
-							output += '<div class="info">';
-								output += '<div class="details">';
-									output += '<a href="#" title="' + data[i].info.title + '">' + data[i].info.title + '</a>';
-								output += '</div>';
-								output += '<div class="price">' + data[i].info.price + '</div>';
-							output += '</div>';
-							output += '<div class="more">';
-								output += '<span class="title">' + data[i].more.title + '</span>';
-								output += '<span class="time">' + data[i].more.time + '</span>';
-							output += '</div>';
-						output += '</div>';
-					output += '</div>';
-				}
-				$("#items").append($(output));
-				$("#items img").on('load', function() {
-					constructItems();
-				});
-				state = true;
-			},
-			error: function() {
-				console.log('Error Loading Data!');
-			}
-		});
-	}
-	loadItems();
-
-	$items.delegate(".item", 'click', function(e) {
-		prepareItemModal();
-		var output = '';
-		var $item = $(this);
-		var itemLink = $item.find('.details a:first').attr('href');
-		var itemId = $item.attr('data-id');
-		var image = $(this).find(".image img").attr('src');
-		var largeImage = createLargeImage(image);
-		var title = $(this).find(".details a").text();
-		if (typeof(itemId) != "undefined" && itemId) {
+	var items = {
+		loadList: function () {
+			output = '';
 			$.ajax({
-				url: 'data/json.php?type=details&id=' + itemId,
+				url: 'data/items.php',
 				dataType: 'json',
 				cache: false,
-				success: function(d) {
-					d.shipping = shipping[parseInt(d.shipping)];
-					d.img = image;
-					d.limage = largeImage;
-					d.title = title;
-					$("#items-modal").find(".modal-header h3").text(title);
-					var detailsTemplate = Handlebars.compile(dTemp);
-					var output = detailsTemplate(d);
-					$("#items-modal").find(".modal-body").html(output);
-					$(".zoom").zoom({url: $(".zoom").find('img:first').attr('data-zoom-image')});
-					$("#items-modal").find("#more-info a:first").attr('href', 'item.php?id=' + itemId);
+				success: function(data) {
+					// console.log(data);
+					for (var i = 0; i < data.length; i++) {
+						// console.log(data[i]);
+						output += '<div class="item" data-id="' + data[i].id + '">';
+							output += '<div class="inner">';
+								output += '<div class="image">';
+									output += '<img src="' + data[i].image + '" alt="' + data[i].imagealt + '" />';
+								output += '</div>';
+								output += '<div class="info">';
+									output += '<div class="details">';
+										output += '<a href="#" title="' + data[i].info.title + '">' + data[i].info.title + '</a>';
+									output += '</div>';
+									output += '<div class="price">' + data[i].info.price + '</div>';
+								output += '</div>';
+								output += '<div class="more">';
+									output += '<span class="title">' + data[i].more.title + '</span>';
+									output += '<span class="time">' + data[i].more.time + '</span>';
+								output += '</div>';
+							output += '</div>';
+						output += '</div>';
+					}
+					$("#items").append($(output));
+					$("#items img").on('load', function() {
+						items.construct();
+					});
+					state = true;
 				},
-				error: function() {
-					alert('Error Loading Details!');
+				error: function () {
+					console.log('Error Loading Data!');
 				}
 			});
+		},
+		construct: function () {
+			$("#items").masonry({
+				itemSelector: '.item',
+				isRTL: true,
+				isAnimated: !Modernizr.csstransitions
+			});
+		},
+		prepareModal: function (e) {
+			$("#item-modal").find(".modal-header h3").text('');
+			$("#item-modal").find(".modal-body").html('');
+			$("#item-modal").modal("show");
+			e.preventDefault();
+		},
+		initZoom: function (img) {
+			var parts = img.split('/');
+			var partsCount = parts.length;
+			parts[partsCount - 1] = 'l_' + parts[partsCount - 1];
+			largeImage = parts.join('/');
+			return largeImage;
+		},
+		load: function($obj) {
+			var output = '';
+			var $item = $obj;
+			var itemLink = $item.find(".details a:first").attr('href');
+			var itemId = $item.attr("data-id");
+			var image = $item.find(".image img").attr('src');
+			var largeImage = items.initZoom(image);
+			var title = $item.find(".details a").text();
+			if (typeof(itemId) != "undefined" && itemId) {
+				$.ajax({
+					url: 'data/json.php?type=details&id=' + itemId,
+					dataType: 'json',
+					cache: false,
+					success: function(d) {
+						d.shipping = shipping[parseInt(d.shipping)];
+						d.img = image;
+						d.limage = largeImage;
+						d.title = title;
+						$("#item-modal").find(".modal-header h3").text(title);
+						var detailsTemplate = Handlebars.compile(dTemp);
+						var output = detailsTemplate(d);
+						$("#item-modal").find(".modal-body").html(output);
+						$(".zoom").zoom({url: $(".zoom").find('img:first').attr('data-zoom-image')});
+						$("#item-modal").find("#more-info a:first").attr('href', 'item.php?id=' + itemId);
+						$("#item-modal").find("#add-cart a:first").attr('href', 'javascript:addToCart(' + itemId + ');');
+					},
+					error: function() {
+						alert('Error Loading Details!');
+					}
+				});
+			}
 		}
-		e.preventDefault();
+	};
+	items.loadList();
+
+	$items.delegate(".item", 'click', function(e) {
+		items.prepareModal(e);
+		items.load($(this));
 	});
 	
-	function prepareItemModal() {
-		$("#items-modal").find(".modal-header h3").text('');
-		$("#items-modal").find(".modal-body").html('');
-		$("#items-modal").modal("show");
-	}
 	
-	function createLargeImage(img) {
-		var parts = img.split('/');
-		var partsCount = parts.length;
-		parts[partsCount - 1] = 'l_' + parts[partsCount - 1];
-		largeImage = parts.join('/');
-		return largeImage;
-	}
-	
-	// $("#items-modal").delegate(".options a", 'click', function(event) {
-		// event.preventDefault();
-	// });
-	
-	$("body").on('hidden', '.modal', function () { $(this).removeData('modal'); });
 	
 	$("#searchcategories").select2({
 		dropdownCssClass: 'bigdrop',
 		containerCssClass: 'search-selectbox'
 	});
-	/*
-	function destroyItems() {
-		return $items.masonry('destroy');
-	}
-	
-	function reconstructItems() {
-		return $items.masonry('reload');
-	}
-	*/
+
 	function desktopSidenav() {
 		$(".nav-list > li").off('click');
 		if ($(".nav-list").find(".arrow:first").hasClass('icon-down-open')) {
